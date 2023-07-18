240eb407b3d2476204b75736a62ccf96f10bca25
Diogenesdcs <107212403+Diogenesdcs@users.noreply.github.com>
2023-07-17 22:46:56 -0300

    Vetores e arrays

---
+++ src/Principal/Programa16.java
@@ -0,0 +1,32 @@
+
+//Array e vetores
+
+//numero[0] = 1 ; primeiro elemento
+//numero[1] = 2 ; segundo elemento
+//numero[2] = 3 ; terceiro elemento
+//numero[3] = 4 ; quarto elemento
+//numero[4] = 5 ; quinto elemento
+package Principal;
+
+public class Programa16 {
+
+	public static void main(String[] args) {
+		String out;
+	//declarando um vetor 
+		int vetor[];
+		
+		//declarando e especificando um vetor 
+		int numeros[] = new int[5];
+		
+		//declarar especificar e inicializar 
+		int outros_numeros[] = {0,1,5,2}; 
+		String frutas[] = {"manga", "uva", "laranja"};
+		
+		String nomes[] = new String[5];
+		char inteiro[] = new char[5];
+		float caracteres[] = new float[5];
+
+
+};
+
+}
