d4c9eca89ab7c792267bd4997c58d2029854085c
Diogenesdcs <107212403+Diogenesdcs@users.noreply.github.com>
2023-08-02 22:30:36 -0300

    Classes java

---
+++ src/Principal/Produto.java
@@ -0,0 +1,13 @@
+//O nome da Classe inicia com letra maiuscula.
+//O nome não deve conter: acentuação,caractere especiais, espaço. 
+//Nas classes javas não existe a implementação da função Main(). 
+//Tofa classe java possui a seguinte forma: O nome da classe java DEVE conter o mesmo nome do arquivo.java. 
+//Tudo que estiver dentro das chaves faz parte da classe.
+
+
+
+package Principal;
+
+public class Produto {
+
+}
